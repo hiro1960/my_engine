0b972aedd34b0449c452e32f36a295d4452e4103
hiro1960 <hoge@exaple.com>
2022-03-05 10:38:18 +0900

    シナリオのSimCtrl部の読み込み作成

--- src/simframe/init.rs
+++ src/simframe/init.rs
@@ -21,5 +21,20 @@ pub fn initialize(db: &mut simframe::sim_data::SimData) {
     let contents = fs::read_to_string(env_file).unwrap();
     let vv: Value = serde_json::from_str(&contents).unwrap();
 
-    println!("in initialize()");
+    println!("in initialize() , {}", vv);
+
+    // SimCtrl　シミュレーション制御情報セット
+    let vs = &vv["SimCtrl"];
+    println!("SimCtrl, {}", vs);
+    let sim_ctrl: String = vs.to_string().parse().unwrap();
+    let v2: Value = serde_json::from_str(&sim_ctrl).unwrap();
+    let ct_value = &v2["Count"];
+    let ct: i64 =  ct_value.to_string().parse().unwrap();
+    let dt_value = &v2["DeltaTime"];
+    let dt: f64 = dt_value.to_string().parse().unwrap();
+    db.time_set.set(ct, dt);
+
+
+    println!("test");
+
 }
